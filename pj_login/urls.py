062278579517bb638db7afc42c640c8e2489a80b
unohayato <mamidori1p1p@gmail.com>
2022-08-17 19:24:38 +0900

    signup url

--- pj_login/urls.py
+++ pj_login/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-
+from registration import veiws, views
 index_view = TemplateView.as_view(template_name="registration/index.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', login_required(index_view), name='index'),
     path('', include('django.contrib.auth.urls')),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
